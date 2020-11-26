17034fa703f6a8324d177d31fa3ad1dc49427657
Paul "Enfield <enfield.0010@gmail.com>
2020-11-25 18:50:11 -0800

    JS Start

---
+++ _scripts/main.js
@@ -0,0 +1,32 @@
+//DROPDOWN MENU
+//Find the element in the HTML with an ID of "hamburger." An "Event Listener" is added that, when "hamburger" is clicked, the following happens.
+document.getElementById("hamburger").addEventListener("click", function(){
+    //querySelectorAll returns an array of HTML elements. A loop is needed to apply the changes to every element.
+	var mod = document.querySelectorAll(".menumodifier");
+    var i;
+	for (i = 0; i < mod.length; i++) {
+		mod[i].classList.toggle("menutoggle") ;
+	}
+});
+
+document.getElementById("mobilelegal").addEventListener("click", function(){
+    //querySelectorAll returns an array of HTML elements. A loop is needed to apply the changes to every element.
+	var mod = document.querySelectorAll(".menumodifier");
+    var i;
+	for (i = 0; i < mod.length; i++) {
+		mod[i].classList.toggle("menutoggle") ;
+	}
+});
+
+//DROPDOWN AUTO-CLOSE
+/*If the menu is still open while the browser window is resized, the menu items will still have the menutoggle class. This will automatically
+remove the menutoggle class if the window hits the breakpoint.*/
+window.addEventListener("resize", function(){
+    if (window.innerWidth > 800) {
+        var mod = document.querySelectorAll(".menutoggle");
+        var i;
+        for (i = 0; i < mod.length; i++) {
+            mod[i].classList.remove("menutoggle") ;
+        }
+    }
+})
